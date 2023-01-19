f6491bb30e3230a26859c0f4f3619fff28641ce9
Farrel Putra <frl25ap@gmail.com>
2023-01-20 03:54:38 +0700

    fix implementation of Base64

--- src/farrel/putra/classes/Base64App.java
+++ src/farrel/putra/classes/Base64App.java
@@ -1,18 +1,21 @@
 package farrel.putra.classes;
 
+import java.util.Arrays;
 import java.util.Base64;
 
 public class Base64App {
     public static void main(String[] args) {
         String ori = "Farrel Athaillah Putra";
+        System.out.println("\nori: " + ori);
 
         String encoded = Base64.getEncoder().encodeToString(ori.getBytes());
-        System.out.println(encoded);
+        System.out.println("\nencoded: " + encoded);
 
         byte[] bytes = Base64.getDecoder().decode(encoded);
-        System.out.println(bytes);
+        System.out.println("\nbytes: " + bytes);
+        System.out.println("Arrays.toString(bytes): " + Arrays.toString(bytes));
 
         String result = new String(bytes);
-        System.out.println(result);
+        System.out.println("\nresult: " + result);
     }
 }
